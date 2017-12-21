0568a71cebed21a3f44e6f70bc4351716211bdb2
rathereasy@gmail.com <jacques@decodemtl>
2017-12-20 20:48:25 -0500

    Question 9

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     let item = this.inp.value
     fetch("http://localhost:3001/addTodo", {
       method: "POST",
-      body: JSON.stringify({item: item, username: this.username})
+      body: JSON.stringify({ item: item, username: this.username })
     });
     this.setState(st => { return { todos: st.todos.concat(item) } }, () => item = "")
   }
@@ -25,17 +25,37 @@ class App extends Component {
     this.setState({ todos: [] })
   }
 
-  componentDidMount() {
-    this.username = window.prompt('what is your username');
+
+  verifyLogin = (username, password) => {
+    return fetch("http://localhost:3001/login", {
+      method: "POST",
+      body: JSON.stringify({username: this.username, password: password})
+    })
+    .then(x => x.text())
+    .then(res => {
+      if(res === "ok") return;
+      this.setState({loginFailed: true});
+    })
+  }
+
+  populateTodos = () => {
     fetch("http://localhost:3001/todos?username=" + this.username)
     .then(x => x.text())
     .then(raw => {
       let lst = JSON.parse(raw);
-      this.setState({todos: lst})
+      this.setState({ todos: lst })
     })
   }
 
+  componentDidMount() {
+    this.username = window.prompt('what is your username');
+    let password = window.prompt('what is your password');
+    this.verifyLogin(this.username, password)
+      .then(this.populateTodos)
+  }
+
   render() {
+    if(this.state.loginFailed) return (<h1> login failed! </h1>);
     return (
       <div className="App">
         <input ref={r => this.inp = r}></input>
